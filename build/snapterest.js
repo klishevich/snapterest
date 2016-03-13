1ad514913b48dd97fabf85218625a90f6c9303b1
klishevich <busation@gmail.com>
2016-03-13 16:39:11 +0300

    statefull component

--- build/snapterest.js
+++ build/snapterest.js
@@ -19037,16 +19037,29 @@ var ReactDOM = require('react-dom');
 var ReactClass = React.createClass({
 	displayName: 'ReactClass',
 
+	getInitialState: function () {
+		return {
+			isHeaderHidden: false
+		};
+	},
+	handleClick: function () {
+		this.setState({
+			isHeaderHidden: !this.state.isHeaderHidden
+		});
+	},
 	render: function () {
-		if (this.props.isHidden) {
-			return null;
+		var title = 'Statefull React Component';
+		var headerElement = React.createElement('h1', { className: 'header', key: 'header' }, title);
+		var buttonElement = React.createElement('button', { className: 'btn btn-default', key: 'button',
+			onClick: this.handleClick }, 'Toggle Header');
+		if (this.state.isHeaderHidden) {
+			return React.createElement('div', { className: 'container' }, buttonElement);
 		}
-		header = this.props.tweets.length + ' Latest tweets';
-		return React.createElement('h1', { className: 'header' }, header);
+		return React.createElement('div', { className: 'container' }, [buttonElement, headerElement]);
 	}
 });
-var tweets_array = ["first", "second", "third"];
-var reactComponentElement = React.createElement(ReactClass, { tweets: tweets_array, isHidden: false });
+// var tweets_array = ["first", "second", "third"];
+var reactComponentElement = React.createElement(ReactClass, { isHidden: false });
 // var listOfItems = <ul className="ul1">
 //                     <li className="li1" key="li1">List Element JSX 1</li>
 //                     <li className="li2" key="li2">List Element JSX 2</li>
