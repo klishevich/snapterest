defd19bf284bcd740a5a4d4a2fde0046f2afe8ce
klishevich <busation@gmail.com>
2016-04-05 00:08:04 +0300

    StreamTweet

---
+++ source/components/StreamTweet.react.js
@@ -0,0 +1,45 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Header = require('./Header.react');
+var Tweet = require('./Tweet.react');
+
+var StreamTweet = React.createClass({
+	getInitialState: function(){
+		console.log('[Snapterest] StreamTweet: 1. Running getInitialState()');
+		return {
+			numberOfCharactersIsIncreasing: null,
+			headerText: null
+		};
+	},
+	componentWillMount: function(){
+		console.log('[Snapterest] StreamTweet: 2. Running componentWillMount()');
+		this.setState({
+			numberOfCharactersIsIncreasing: true,
+			headerText: 'Latest public foto from Tweeter'
+		});
+		window.snapterest = {
+			numberOfRecievedTweets: 1,
+			numberOfDisplayedTweets: 1
+		};
+	},
+	componentDidMount: function(){
+		console.log('[Snapterest] StreamTweet: 3. Running componentDidMount()');
+		var componentDOMRepresentation = ReactDOM.findDOMNode(this);
+		window.snapterest.headerHtml = componentDOMRepresentation.children[0].outerHTML;
+		window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
+	},
+	render: function(){
+		console.log('[Snapterest] StreamTweet: Running render()');
+
+		return (
+			<section>
+				<Header text={this.state.headerText} />
+				<Tweet
+					tweet={this.props.tweet}
+					onmageClick={this.props.onAddTweetToCollection}/>
+			</section>
+			);
+	}
+});
+
+module.exports = StreamTweet;
